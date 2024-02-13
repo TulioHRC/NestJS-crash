3782acf38136fe9baa13f9c48f1422d5e120475f
TulioHRC <grove1411@gmail.com>
2024-02-12 22:54:27 -0300

    feat: added getSpecificProduct, update and remove
    I finished the 1 hour crash course video, and I loved it.

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,23 +1,36 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, DefaultValuePipe, Get, Param, Patch, Post, Query, Delete } from "@nestjs/common";
 import { ProductsService } from "./products.service";
 import { Product } from "./products.model";
+import { ProductDTO } from "./DTO/products.dto";
 
 @Controller('products')
 export class ProductsController {
     constructor(private readonly productsService : ProductsService) {}
 
     @Post()
-    addProduct(
-        @Body('title') productTitle: string, 
-        @Body('description') productDesc: string, 
-        @Body('price') productPrice: number
-    ) : {id: string} {
-        const productId = this.productsService.addProduct(productTitle, productDesc, productPrice);
+    addProduct(@Body() body: ProductDTO) : {id: string} { // DTO requires some data structure
+        const productId = this.productsService.addProduct(body.title, body.description, body.price);
         return {id: productId};
     }
 
     @Get()
-    getAllProducts() : Product[] {
-        return this.productsService.getAllProducts();
+    //getSpecificProduct(@Param('id') productId: string) : Product { // @Param use the "/", @Query "?"
+    getProducts(@Query('id', new DefaultValuePipe("")) productId: string) {
+        return this.productsService.getProducts(productId);
+    }
+
+    @Patch(":id")
+    updateProduct(
+        @Param('id') id: string, 
+        @Body('title') title: string,
+        @Body('description') description: string,
+        @Body('price') price: number
+    ) : string {
+        return this.productsService.updateProduct(id, title, description, price);
+    }
+
+    @Delete(":id")
+    removeProduct(@Param('id') id: string) : string {
+        return this.productsService.removeProduct(id);
     }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { Product } from "./products.model"; // Product class model
 import { randomUUID } from "crypto";
 
@@ -14,8 +14,53 @@ export class ProductsService {
         return newProductId;
     }
 
-    getAllProducts() {
+    getProducts(productId: string) {
+        if(productId) 
+            return this.getProductById(productId);
+        else 
+            return this.getAllProducts();
+    }
+
+    updateProduct(productId: string, title: string, desc: string, price: number) {
+        const productIndex = this.getProductIndexById(productId);
+        const oldProduct = this.products[productIndex];
+        const updatedProduct = {...oldProduct};
+        
+        // Optional
+        if(title) updatedProduct.title = title;
+        if(desc) updatedProduct.description = desc;
+        if(price) updatedProduct.price = price;
+
+        this.products[productIndex] = updatedProduct;
+
+        return "Product updated.";
+    }
+
+    removeProduct(id: string) {
+        const productIndex = this.getProductIndexById(id);
+        this.products.splice(productIndex, 1);
+
+        return "Product deleted.";
+    }
+
+    // Private methods
+
+    private getAllProducts() {
         // return this.products; That is returning the pointer of products (so the controller will be able to modify products), instead:
         return [...this.products.map(product => ({...product}))]; // Copy of products (each product being a copy) Deep copy
+    }
+
+    private getProductById(productId: string) {
+        const product = this.products.find(product => product.id == productId);
+        if(!product)
+            throw new NotFoundException("Could not find product."); // 404 error
+        return {...product};
+    }
+
+    private getProductIndexById(productId: string) {
+        const productIndex = this.products.findIndex(product => product.id==productId);
+        if(productIndex == -1)
+            throw new NotFoundException("Could not find product.");
+        return productIndex;
     }
 }
